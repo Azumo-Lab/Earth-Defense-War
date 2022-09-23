7016137a38388ee90c9fbd3e256fc318178a76fc
牛奶 <burpee_soggier0x@icloud.com>
2022-09-23 17:40:55 +0800

    更新：初始化代码

---
+++ Earth Defense War/Earth Defense War/Extension.Canvas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Earth_Defense_War
+{
+    internal static partial class Extension
+    {
+        public static void AddToCanvas(this Canvas canvas, UIElement element)
+        {
+            canvas.Children.Add(element);
+        }
+    }
+}
---
+++ Earth Defense War/Earth Defense War/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Earth_Defense_War
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitGame();
+        }
+
+        #region 游戏界面的初始化
+        
+        public void InitGame()
+        {
+            Button ExitButton = new Button()
+            {
+                Content = "退出",
+                Height = 50,
+                Width = 100,
+            };
+            ExitButton.Click += new RoutedEventHandler((obj, e) =>
+            {
+                MessageBox.Show("退出");
+                Close();
+            });
+            GameCanvas.AddToCanvas(ExitButton);
+            Canvas.SetLeft(ExitButton, 100);
+            Canvas.SetTop(ExitButton, 100);
+        }
+        #endregion
+    }
+}
